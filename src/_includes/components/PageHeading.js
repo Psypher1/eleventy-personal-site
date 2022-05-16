9e119505e612e0340ab328fcd128863214236255
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-05-16 13:26:19 +0200

    modified styling

--- src/_includes/components/PageHeading.js
+++ src/_includes/components/PageHeading.js
@@ -2,7 +2,9 @@ const { html } = require("common-tags");
 
 function PageHeading({ heading, subheading }) {
   return html`
-    <h1 class="mb-2 text-4xl font-heading sm:text-4xl md:mb-3 md:text-5xl">
+    <h1
+      class="mb-2 text-4xl font-heading sm:text-4xl md:mb-3 md:text-5xl text-gray-100"
+    >
       ${heading}
     </h1>
 
