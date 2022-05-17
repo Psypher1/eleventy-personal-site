18ac4e7849b1b6dcb126290302641412dffd8a40
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-05-17 21:15:10 +0200

    using pagesect componet

--- src/_includes/components/PageSection.js
+++ src/_includes/components/PageSection.js
@@ -1,6 +1,6 @@
 const PageSection = (content, sectionHeading) => {
   return `<section class="mt-16">
-    <h2 class="mb-4 font-headingAlt text-2xl font-semibold text-gray-100 md:text-3xl">${sectionHeading}</h2>
+    <h2 class="mb-3 font-headingAlt text-2xl font-semibold text-gray-100 md:text-3xl">${sectionHeading}</h2>
     ${content}
     </section>`;
 };
