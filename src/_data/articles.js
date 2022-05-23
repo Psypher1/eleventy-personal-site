99edc5868fcaf581f5f2a6ec02bde6d2f48de592
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-05-23 16:33:40 +0200

    added parchment

--- src/_data/articles.js
+++ src/_data/articles.js
@@ -1,1 +1,32 @@
 require("dotenv").config();
+
+const endpoint = `https://api.hashnode.com/`;
+
+module.exports = async () => {
+  const { GraphQLClient, gql } = require("graphql-request");
+
+  const client = new GraphQLClient(endpoint);
+
+  const query = gql`
+    {
+      user(username: "Psypher1") {
+        publication {
+          posts {
+            title
+            coverImage
+            brief
+            slug
+            dateAdded
+            contentMarkdown
+          }
+        }
+      }
+    }
+  `;
+
+  const articles = await client.request(query);
+
+  console.log(articles);
+
+  return articles.user.publication.posts;
+};
