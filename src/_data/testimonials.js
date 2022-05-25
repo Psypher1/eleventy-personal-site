16ecbe8c9947d378923d31549537128fe680260e
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-05-25 18:16:43 +0200

    index

--- src/_data/testimonials.js
+++ src/_data/testimonials.js
@@ -5,8 +5,9 @@ module.exports = [
     link: "https://www.facebook.com/kuziwa.dante"
   },
   {
-    name: "Tapiwanashe",
-    message: "Dante is goood",
+    name: "Tapiwanashe Manhombo",
+    message:
+      "Dante goes above and beyond expectations each time. He will call you to verify things or run ideas past you. And he will make sure everything runs as efficiently as possible. He NEVER forgets a discussion, which is great because nothing falls through the cracks. Highly recommend.",
     link: "https://twitter.com/tapiwanashe36"
   }
 ];
