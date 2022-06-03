6d3edd7fcc4db773b41882e310c34ce7a6dd42c5
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-06-03 10:02:51 +0200

    removed role stuff from tags

--- src/_includes/components/PageSection.js
+++ src/_includes/components/PageSection.js
@@ -1,5 +1,5 @@
 const PageSection = (content, sectionHeading) => {
-  return `<section role="section" class="mt-16">
+  return `<section  class="mt-16">
     <h2 class="mb-4 font-headingAlt text-2xl font-semibold text-gray-100 md:text-3xl">${sectionHeading}</h2>
     ${content}
     </section>`;
