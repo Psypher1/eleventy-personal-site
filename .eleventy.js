66615dc17a59481701c384cd0fb789e371a46406
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-07-19 16:32:08 +0200

    changed so articles load on site and not on external site

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,22 @@
 const { DateTime } = require("luxon");
 const Image = require("@11ty/eleventy-img");
+const { parseISO, format } = require("date-fns");
+const markdownIt = require("markdown-it");
 
 // Shortcode Imports
 const PageHeading = require("./src/_includes/components/PageHeading.js");
 const PageSection = require("./src/_includes/components/PageSection.js");
 
-module.exports = function(config) {
+module.exports = function (config) {
+  // Markdown Plugins
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  };
+
+  config.setLibrary("md", markdownIt(options));
+
   // config.addPassthroughCopy("src/assets/css/**/*");
   config.addPassthroughCopy("src/assets/css/index.css");
 
@@ -24,15 +35,25 @@ module.exports = function(config) {
     return date;
   });
 
-  config.addFilter("articleDate", dateObj => {
+  config.addFilter("pubDate", (date) => {
+    const dateString = parseISO(date, "yyyy-MM-dd");
+    const formattedDate = format(dateString, "dd MMM, yyyy");
+    return `${formattedDate}`;
+  });
+
+  config.addFilter("articleDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj)
       .setLocale("uk")
       .toLocaleString(DateTime.DATE_MED);
   });
 
+  config.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
   // Page Ordering
-  config.addCollection("page", function(collections) {
-    return collections.getFilteredByTag("page").sort(function(a, b) {
+  config.addCollection("page", function (collections) {
+    return collections.getFilteredByTag("page").sort(function (a, b) {
       return a.data.order - b.data.order;
     });
   });
@@ -45,23 +66,23 @@ module.exports = function(config) {
   config.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   // shortcode
-  config.addPairedNunjucksShortcode("section", function(
-    content,
-    sectionHeading
-  ) {
-    return `<section class="mt-16">
-    <h2 class="mb-4 font-headingAlt text-2xl font-semibold text-gray-100 md:text-3xl">${sectionHeading}</h2>
-    ${content}
-    </section>`;
-  });
+  // config.addPairedNunjucksShortcode("section", function(
+  //   content,
+  //   sectionHeading
+  // ) {
+  //   return `<section class="mt-16">
+  //   <h2 class="mb-4 font-headingAlt text-2xl font-semibold text-gray-100 md:text-3xl">${sectionHeading}</h2>
+  //   ${content}
+  //   </section>`;
+  // });
 
   return {
     dir: {
       input: "src",
       data: "_data",
       includes: "_includes",
-      layouts: "_layouts"
-    }
+      layouts: "_layouts",
+    },
   };
 };
 
